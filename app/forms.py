3c696ef5c7df926d29822d184db9c233bf8ffa37
bober <boberstarosta@gmail.com>
2018-11-21 08:11:22 +0100

    handling existing username in edit profile form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
+from wtforms.validators import Email, EqualTo, Length
 from app.models import User
 
 
@@ -34,3 +36,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
