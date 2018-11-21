630ef6d7552c2c4a04124332b717f1497e338925
bober <boberstarosta@gmail.com>
2018-11-21 20:52:42 +0100

    added time format with locale

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_babel import Babel
 from flask_babel import lazy_gettext as _l
 
@@ -22,6 +23,7 @@ login.login_view = 'login'
 login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 babel = Babel(app)
 
 if not app.debug:
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from flask import render_template, flash, redirect, request, url_for
+from flask import g
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_babel import _
+from flask_babel import get_locale
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
@@ -15,6 +17,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])
