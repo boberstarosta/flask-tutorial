9f7f9275e3199d5b447b89f2bddc3e086239c5a4
boberstarosta <boberstarosta@gmail.com>
2018-11-22 12:46:29 +0100

    fixed translate bug

--- app/translate.py
+++ app/translate.py
@@ -5,8 +5,8 @@ from flask_babel import _
 
 
 def translate(text, source_language, dest_language):
-    if 'MS_TRANSLATOR_KEY' not in app.config or \
-            not app.config['MS_TRANSLATOR_KEY']:
+    if 'MS_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
 
     auth = {
